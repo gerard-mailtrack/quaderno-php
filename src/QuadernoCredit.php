0ebedecfc4ee6bee1ba37d8aee06a23e7c0322ab
Gerard Ruiz <gerard@mailtrack.io>
2015-10-27 10:24:45 +0100

    Fixes phpdoc

--- src/QuadernoCredit.php
+++ src/QuadernoCredit.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Quaderno Invoice
-*
-* @package   Quaderno PHP
-* @author    Quaderno <hello@quaderno.io>
-* @copyright Copyright (c) 2015, Quaderno
-* @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
-*/
+ * Quaderno Credit
+ *
+ * @package   Quaderno PHP
+ * @author    Quaderno <hello@quaderno.io>
+ * @copyright Copyright (c) 2015, Quaderno
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
 
 class QuadernoCredit extends QuadernoDocument
 {
--- src/QuadernoRecurring.php
+++ src/QuadernoRecurring.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Quaderno Invoice
-*
-* @package   Quaderno PHP
-* @author    Quaderno <hello@quaderno.io>
-* @copyright Copyright (c) 2015, Quaderno
-* @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
-*/
+ * QuadernoRecurring
+ *
+ * @package   Quaderno PHP
+ * @author    Quaderno <hello@quaderno.io>
+ * @copyright Copyright (c) 2015, Quaderno
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
 
 class QuadernoRecurring extends QuadernoDocument
 {
